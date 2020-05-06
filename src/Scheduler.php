7722ae32ff115a6d20a6127de7252101b01250c4
Thomas Alrek <thomas@alrek.no>
2020-05-06 14:15:50 +0200

    Update Scheduler

--- src/Scheduler.php
+++ src/Scheduler.php
@@ -2,120 +2,380 @@
 
 namespace Netflex\Scheduler;
 
+use Closure;
+use DateTimeInterface;
+
+use Carbon\Carbon;
+
 use Netflex\API\Facades\API;
+use Netflex\Foundation\Variable;
+use Netflex\Support\JWT;
+
+use Illuminate\Container\Container;
 use Illuminate\Contracts\Queue\Queue;
+use Illuminate\Queue\CallQueuedClosure;
+use Illuminate\Support\InteractsWithTime;
+use Illuminate\Support\Str;
 
 class Scheduler implements Queue {
-    /**
-     * Get the size of the queue.
-     *
-     * @param  string|null  $queue
-     * @return int
-     */
-    public function size($queue = null) {
-      return API::get('scheduler/jobs/size')->size;
-    }
+  use InteractsWithTime;
 
-    /**
-     * Push a new job onto the queue.
-     *
-     * @param  string|object  $job
-     * @param  mixed  $data
-     * @param  string|null  $queue
-     * @return mixed
-     */
-    public function push($job, $data = '', $queue = null) {
+  /**
+   * The connection name for the queue.
+   *
+   * @var string
+   */
+  protected $connectionName;
 
-    }
+  /**
+   * The IoC container instance.
+   *
+   * @var \Illuminate\Container\Container
+   */
+  protected $container;
 
-    /**
-     * Push a new job onto the queue.
-     *
-     * @param  string  $queue
-     * @param  string|object  $job
-     * @param  mixed  $data
-     * @return mixed
-     */
-    public function pushOn($queue, $job, $data = '') {
+  /**
+   * The URL endpoints to dispatch the job to
+   *
+   * @var string
+   */
+  protected $url;
 
-    }
+  /**
+   * The create payload callbacks.
+   *
+   * @var callable[]
+   */
+  protected static $createPayloadCallbacks = [];
 
-    /**
-     * Push a raw payload onto the queue.
-     *
-     * @param  string  $payload
-     * @param  string|null  $queue
-     * @param  array  $options
-     * @return mixed
-     */
-    public function pushRaw($payload, $queue = null, array $options = []) {
+  public function __construct($url)
+  {
+      $this->url = $url;
+  }
 
-    }
+  /**
+   * Get the size of the queue.
+   *
+   * @param  string|null  $queue
+   * @return int
+   */
+  public function size($queue = null)
+  {
+      return API::get("scheduler/queue/{$this->getConnectionName()}/size")->size;
+  }
 
-    /**
-     * Push a new job onto the queue after a delay.
-     *
-     * @param  \DateTimeInterface|\DateInterval|int  $delay
-     * @param  string|object  $job
-     * @param  mixed  $data
-     * @param  string|null  $queue
-     * @return mixed
-     */
-    public function later($delay, $job, $data = '', $queue = null) {
+  /**
+   * Push a new job onto the queue.
+   *
+   * @param  string|object  $job
+   * @param  mixed  $data
+   * @param  string|null  $queue
+   * @return mixed
+   */
+  public function push($job, $data = '', $queue = null)
+  {
+      $payload = $this->createPayload($job, $this->getQueue($queue), $data);
 
-    }
+      return $this->pushRaw($payload, $this->getQueue($queue), [
+          'start' => Carbon::now()->toDateTimeString()
+      ]);
+  }
 
-    /**
-     * Push a new job onto the queue after a delay.
-     *
-     * @param  string  $queue
-     * @param  \DateTimeInterface|\DateInterval|int  $delay
-     * @param  string|object  $job
-     * @param  mixed  $data
-     * @return mixed
-     */
-    public function laterOn($queue, $delay, $job, $data = '') {
+  protected function getQueue ($queue = null) {
+      return 'default';
+  }
 
-    }
+  /**
+   * Push a new job onto the queue.
+   *
+   * @param  string  $queue
+   * @param  string|object  $job
+   * @param  mixed  $data
+   * @return mixed
+   */
+  public function pushOn($queue, $job, $data = '')
+  {
+      return $this->push($job, $data, $queue);
+  }
 
-    /**
-     * Push an array of jobs onto the queue.
-     *
-     * @param  array  $jobs
-     * @param  mixed  $data
-     * @param  string|null  $queue
-     * @return mixed
-     */
-    public function bulk($jobs, $data = '', $queue = null) {
+  /**
+   * Push a raw payload onto the queue.
+   *
+   * @param  string  $payload
+   * @param  string|null  $queue
+   * @param  array  $options
+   * @return mixed
+   */
+  public function pushRaw($payload, $queue = null, array $options = [])
+  {
+      return API::post('scheduler/jobs', [
+          'method' => 'post',
+          'name' => $payload['displayName'] . '(' . $payload['uuid'] . ')',
+          'url' => $this->url . '/.well-known/netflex?token=' . JWT::create($payload, Variable::get('netflex_api')),
+          'payload' => $payload,
+          'start' => $options['start'] ?? Carbon::now()->toDateTimeString(),
+          'enabled' => true
+      ]);
+  }
 
-    }
+  /**
+   * Push a new job onto the queue after a delay.
+   *
+   * @param  \DateTimeInterface|\DateInterval|int  $delay
+   * @param  string|object  $job
+   * @param  mixed  $data
+   * @param  string|null  $queue
+   * @return mixed
+   */
+  public function later($delay, $job, $data = '', $queue = null)
+  {
+      $payload = $this->createPayload($job, $this->getQueue($queue), $data);
+      $start = Carbon::createFromTimestamp($this->availableAt($delay))->toDateTimeString();
 
-    /**
-     * Pop the next job off of the queue.
-     *
-     * @param  string|null  $queue
-     * @return \Illuminate\Contracts\Queue\Job|null
-     */
-    public function pop($queue = null) {
+      return $this->pushRaw($payload, $this->getQueue($queue), [
+          'start' => $start
+      ]);
+  }
 
-    }
+  /**
+   * Push a new job onto the queue after a delay.
+   *
+   * @param  string  $queue
+   * @param  \DateTimeInterface|\DateInterval|int  $delay
+   * @param  string|object  $job
+   * @param  mixed  $data
+   * @return mixed
+   */
+  public function laterOn($queue, $delay, $job, $data = '')
+  {
+      return $this->later($delay, $job, $data, $queue);
+  }
 
-    /**
-     * Get the connection name for the queue.
-     *
-     * @return string
-     */
-    public function getConnectionName() {
-      return 'jobs';
-    }
+  /**
+   * Push an array of jobs onto the queue.
+   *
+   * @param  array  $jobs
+   * @param  mixed  $data
+   * @param  string|null  $queue
+   * @return mixed
+   */
+  public function bulk($jobs, $data = '', $queue = null)
+  {
+      foreach ((array) $jobs as $job) {
+          $this->push($job, $data, $queue);
+      }
+  }
 
-    /**
-     * Set the connection name for the queue.
-     *
-     * @param  string  $name
-     * @return $this
-     */
-    public function setConnectionName($name) {
+  /**
+   * Create a payload string from the given job and data.
+   *
+   * @param  \Closure|string|object  $job
+   * @param  string  $queue
+   * @param  mixed  $data
+   * @return string
+   *
+   * @throws \Illuminate\Queue\InvalidPayloadException
+   */
+  protected function createPayload($job, $queue, $data = '')
+  {
+      if ($job instanceof Closure) {
+          $job = CallQueuedClosure::create($job);
+      }
+
+      return $this->createPayloadArray($job, $queue, $data);
+  }
+
+  /**
+   * Create a payload array from the given job and data.
+   *
+   * @param  string|object  $job
+   * @param  string  $queue
+   * @param  mixed  $data
+   * @return array
+   */
+  protected function createPayloadArray($job, $queue, $data = '')
+  {
+      return is_object($job)
+          ? $this->createObjectPayload($job, $queue)
+          : $this->createStringPayload($job, $queue, $data);
+  }
+
+  /**
+   * Create a payload for an object-based queue handler.
+   *
+   * @param  object  $job
+   * @param  string  $queue
+   * @return array
+   */
+  protected function createObjectPayload($job, $queue)
+  {
+      $payload = $this->withCreatePayloadHooks($queue, [
+          'uuid' => (string) Str::uuid(),
+          'displayName' => $this->getDisplayName($job),
+          'job' => 'Illuminate\Queue\CallQueuedHandler@call',
+          'maxTries' => $job->tries ?? null,
+          'maxExceptions' => $job->maxExceptions ?? null,
+          'delay' => $this->getJobRetryDelay($job),
+          'timeout' => $job->timeout ?? null,
+          'timeoutAt' => $this->getJobExpiration($job),
+          'data' => [
+              'commandName' => $job,
+              'command' => $job,
+          ],
+      ]);
+
+      return array_merge($payload, [
+          'data' => [
+              'commandName' => get_class($job),
+              'command' => serialize(clone $job),
+          ],
+      ]);
+  }
+
+  /**
+   * Get the display name for the given job.
+   *
+   * @param  object  $job
+   * @return string
+   */
+  protected function getDisplayName($job)
+  {
+      return method_exists($job, 'displayName')
+          ? $job->displayName() : get_class($job);
+  }
+
+  /**
+   * Get the retry delay for an object-based queue handler.
+   *
+   * @param  mixed  $job
+   * @return mixed
+   */
+  public function getJobRetryDelay($job)
+  {
+      if (!method_exists($job, 'retryAfter') && !isset($job->retryAfter)) {
+          return;
+      }
+
+      $delay = $job->retryAfter ?? $job->retryAfter();
+
+      return $delay instanceof DateTimeInterface
+          ? $this->secondsUntil($delay) : $delay;
+  }
+
+  /**
+   * Get the expiration timestamp for an object-based queue handler.
+   *
+   * @param  mixed  $job
+   * @return mixed
+   */
+  public function getJobExpiration($job)
+  {
+      if (!method_exists($job, 'retryUntil') && !isset($job->timeoutAt)) {
+          return;
+      }
+
+      $expiration = $job->timeoutAt ?? $job->retryUntil();
+
+      return $expiration instanceof DateTimeInterface
+          ? $expiration->getTimestamp() : $expiration;
+  }
+
+  /**
+   * Create a typical, string based queue payload array.
+   *
+   * @param  string  $job
+   * @param  string  $queue
+   * @param  mixed  $data
+   * @return array
+   */
+  protected function createStringPayload($job, $queue, $data)
+  {
+      return $this->withCreatePayloadHooks($queue, [
+          'uuid' => (string) Str::uuid(),
+          'displayName' => is_string($job) ? explode('@', $job)[0] : null,
+          'job' => $job,
+          'maxTries' => null,
+          'maxExceptions' => null,
+          'delay' => null,
+          'timeout' => null,
+          'data' => $data,
+      ]);
+  }
+
+  /**
+   * Register a callback to be executed when creating job payloads.
+   *
+   * @param  callable  $callback
+   * @return void
+   */
+  public static function createPayloadUsing($callback)
+  {
+      if (is_null($callback)) {
+          static::$createPayloadCallbacks = [];
+      } else {
+          static::$createPayloadCallbacks[] = $callback;
+      }
+  }
+
+  /**
+   * Create the given payload using any registered payload hooks.
+   *
+   * @param  string  $queue
+   * @param  array  $payload
+   * @return array
+   */
+  protected function withCreatePayloadHooks($queue, array $payload)
+  {
+      if (!empty(static::$createPayloadCallbacks)) {
+          foreach (static::$createPayloadCallbacks as $callback) {
+              $payload = array_merge($payload, call_user_func(
+                  $callback,
+                  $this->getConnectionName(),
+                  $queue,
+                  $payload
+              ));
+          }
+      }
+
+      return $payload;
+  }
+
+  /**
+   * Pop the next job off of the queue.
+   *
+   * @param  string|null  $queue
+   * @return \Illuminate\Contracts\Queue\Job|null
+   */
+  public function pop($queue = null)
+  {
+      return null;
+  }
+
+  /**
+   * Get the connection name for the queue.
+   *
+   * @return string
+   */
+  public function getConnectionName()
+  {
+      return $this->connectionName;
+  }
+
+  /**
+   * Set the connection name for the queue.
+   *
+   * @param  string  $name
+   * @return $this
+   */
+  public function setConnectionName($name)
+  {
+      $this->connectionName = $name;
       return $this;
-    }
+  }
+
+  public function setContainer(Container $container)
+  {
+      $this->container = $container;
+      return $this;
+  }
 }
